41af8dd5979dd663ae6a86be0a38339368d7cb21
mosearc <115136308+mosearc@users.noreply.github.com>
2023-11-28 16:55:06 +0100

    servo (working)

---
+++ servo2/Debug/main_nortos.d
@@ -0,0 +1,45 @@
+# FIXED
+
+main_nortos.obj: ../main_nortos.c
+main_nortos.obj: /home/mose/ti/ccs1250/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/stdint.h
+main_nortos.obj: /home/mose/ti/ccs1250/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/_ti_config.h
+main_nortos.obj: /home/mose/ti/ccs1250/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/linkage.h
+main_nortos.obj: /home/mose/ti/ccs1250/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/_stdint40.h
+main_nortos.obj: /home/mose/ti/ccs1250/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/stdint.h
+main_nortos.obj: /home/mose/ti/ccs1250/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/cdefs.h
+main_nortos.obj: /home/mose/ti/ccs1250/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/_types.h
+main_nortos.obj: /home/mose/ti/ccs1250/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/machine/_types.h
+main_nortos.obj: /home/mose/ti/ccs1250/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/machine/_stdint.h
+main_nortos.obj: /home/mose/ti/ccs1250/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/_stdint.h
+main_nortos.obj: /home/mose/ti/ccs1250/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/stddef.h
+main_nortos.obj: /home/mose/Desktop/courses/Embedded/simplelink_msp432p4_sdk_3_40_01_02/kernel/nortos/NoRTOS.h
+main_nortos.obj: /home/mose/Desktop/courses/Embedded/simplelink_msp432p4_sdk_3_40_01_02/source/ti/drivers/Board.h
+
+../main_nortos.c:
+
+/home/mose/ti/ccs1250/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/stdint.h:
+
+/home/mose/ti/ccs1250/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/_ti_config.h:
+
+/home/mose/ti/ccs1250/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/linkage.h:
+
+/home/mose/ti/ccs1250/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/_stdint40.h:
+
+/home/mose/ti/ccs1250/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/stdint.h:
+
+/home/mose/ti/ccs1250/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/cdefs.h:
+
+/home/mose/ti/ccs1250/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/_types.h:
+
+/home/mose/ti/ccs1250/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/machine/_types.h:
+
+/home/mose/ti/ccs1250/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/machine/_stdint.h:
+
+/home/mose/ti/ccs1250/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/_stdint.h:
+
+/home/mose/ti/ccs1250/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/stddef.h:
+
+/home/mose/Desktop/courses/Embedded/simplelink_msp432p4_sdk_3_40_01_02/kernel/nortos/NoRTOS.h:
+
+/home/mose/Desktop/courses/Embedded/simplelink_msp432p4_sdk_3_40_01_02/source/ti/drivers/Board.h:
+
