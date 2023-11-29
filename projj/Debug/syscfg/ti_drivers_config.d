f340427d771969634096b5a37064688ce7d78e57
mosearc <mose.arcaro@gmail.com>
2023-11-29 12:03:07 +0100

    servo working ee buzzer no tested

---
+++ projj/Debug/syscfg/ti_drivers_config.d
@@ -0,0 +1,102 @@
+# FIXED
+
+syscfg/ti_drivers_config.obj: syscfg/ti_drivers_config.c
+syscfg/ti_drivers_config.obj: /home/mose/ti/ccs1250/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/stddef.h
+syscfg/ti_drivers_config.obj: /home/mose/ti/ccs1250/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/_ti_config.h
+syscfg/ti_drivers_config.obj: /home/mose/ti/ccs1250/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/linkage.h
+syscfg/ti_drivers_config.obj: /home/mose/Desktop/courses/Embedded/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/DeviceFamily.h
+syscfg/ti_drivers_config.obj: syscfg/ti_drivers_config.h
+syscfg/ti_drivers_config.obj: /home/mose/ti/ccs1250/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/stdint.h
+syscfg/ti_drivers_config.obj: /home/mose/ti/ccs1250/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/_stdint40.h
+syscfg/ti_drivers_config.obj: /home/mose/ti/ccs1250/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/stdint.h
+syscfg/ti_drivers_config.obj: /home/mose/ti/ccs1250/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/cdefs.h
+syscfg/ti_drivers_config.obj: /home/mose/ti/ccs1250/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/_types.h
+syscfg/ti_drivers_config.obj: /home/mose/ti/ccs1250/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/machine/_types.h
+syscfg/ti_drivers_config.obj: /home/mose/ti/ccs1250/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/machine/_stdint.h
+syscfg/ti_drivers_config.obj: /home/mose/ti/ccs1250/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/_stdint.h
+syscfg/ti_drivers_config.obj: /home/mose/Desktop/courses/Embedded/simplelink_msp432p4_sdk_3_40_01_02/source/ti/drivers/PWM.h
+syscfg/ti_drivers_config.obj: /home/mose/Desktop/courses/Embedded/simplelink_msp432p4_sdk_3_40_01_02/source/ti/drivers/pwm/PWMTimerMSP432.h
+syscfg/ti_drivers_config.obj: /home/mose/ti/ccs1250/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/stdbool.h
+syscfg/ti_drivers_config.obj: /home/mose/Desktop/courses/Embedded/simplelink_msp432p4_sdk_3_40_01_02/source/ti/drivers/Power.h
+syscfg/ti_drivers_config.obj: /home/mose/Desktop/courses/Embedded/simplelink_msp432p4_sdk_3_40_01_02/source/ti/drivers/utils/List.h
+syscfg/ti_drivers_config.obj: /home/mose/Desktop/courses/Embedded/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/driverlib/pmap.h
+syscfg/ti_drivers_config.obj: /home/mose/Desktop/courses/Embedded/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/inc/msp.h
+syscfg/ti_drivers_config.obj: /home/mose/Desktop/courses/Embedded/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/inc/msp432p401r.h
+syscfg/ti_drivers_config.obj: /home/mose/Desktop/courses/Embedded/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/inc/msp_compatibility.h
+syscfg/ti_drivers_config.obj: /home/mose/Desktop/courses/Embedded/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/inc/msp432p401r_classic.h
+syscfg/ti_drivers_config.obj: /home/mose/Desktop/courses/Embedded/simplelink_msp432p4_sdk_3_40_01_02/source/third_party/CMSIS/Include/core_cm4.h
+syscfg/ti_drivers_config.obj: /home/mose/Desktop/courses/Embedded/simplelink_msp432p4_sdk_3_40_01_02/source/third_party/CMSIS/Include/cmsis_version.h
+syscfg/ti_drivers_config.obj: /home/mose/Desktop/courses/Embedded/simplelink_msp432p4_sdk_3_40_01_02/source/third_party/CMSIS/Include/cmsis_compiler.h
+syscfg/ti_drivers_config.obj: /home/mose/Desktop/courses/Embedded/simplelink_msp432p4_sdk_3_40_01_02/source/third_party/CMSIS/Include/cmsis_ccs.h
+syscfg/ti_drivers_config.obj: /home/mose/Desktop/courses/Embedded/simplelink_msp432p4_sdk_3_40_01_02/source/third_party/CMSIS/Include/mpu_armv7.h
+syscfg/ti_drivers_config.obj: /home/mose/Desktop/courses/Embedded/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/inc/system_msp432p401r.h
+syscfg/ti_drivers_config.obj: /home/mose/Desktop/courses/Embedded/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/driverlib/timer_a.h
+syscfg/ti_drivers_config.obj: /home/mose/Desktop/courses/Embedded/simplelink_msp432p4_sdk_3_40_01_02/source/ti/drivers/power/PowerMSP432.h
+syscfg/ti_drivers_config.obj: /home/mose/Desktop/courses/Embedded/simplelink_msp432p4_sdk_3_40_01_02/source/ti/drivers/Board.h
+
+syscfg/ti_drivers_config.c:
+
+/home/mose/ti/ccs1250/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/stddef.h:
+
+/home/mose/ti/ccs1250/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/_ti_config.h:
+
+/home/mose/ti/ccs1250/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/linkage.h:
+
+/home/mose/Desktop/courses/Embedded/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/DeviceFamily.h:
+
+syscfg/ti_drivers_config.h:
+
+/home/mose/ti/ccs1250/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/stdint.h:
+
+/home/mose/ti/ccs1250/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/_stdint40.h:
+
+/home/mose/ti/ccs1250/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/stdint.h:
+
+/home/mose/ti/ccs1250/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/cdefs.h:
+
+/home/mose/ti/ccs1250/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/_types.h:
+
+/home/mose/ti/ccs1250/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/machine/_types.h:
+
+/home/mose/ti/ccs1250/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/machine/_stdint.h:
+
+/home/mose/ti/ccs1250/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/_stdint.h:
+
+/home/mose/Desktop/courses/Embedded/simplelink_msp432p4_sdk_3_40_01_02/source/ti/drivers/PWM.h:
+
+/home/mose/Desktop/courses/Embedded/simplelink_msp432p4_sdk_3_40_01_02/source/ti/drivers/pwm/PWMTimerMSP432.h:
+
+/home/mose/ti/ccs1250/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/stdbool.h:
+
+/home/mose/Desktop/courses/Embedded/simplelink_msp432p4_sdk_3_40_01_02/source/ti/drivers/Power.h:
+
+/home/mose/Desktop/courses/Embedded/simplelink_msp432p4_sdk_3_40_01_02/source/ti/drivers/utils/List.h:
+
+/home/mose/Desktop/courses/Embedded/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/driverlib/pmap.h:
+
+/home/mose/Desktop/courses/Embedded/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/inc/msp.h:
+
+/home/mose/Desktop/courses/Embedded/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/inc/msp432p401r.h:
+
+/home/mose/Desktop/courses/Embedded/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/inc/msp_compatibility.h:
+
+/home/mose/Desktop/courses/Embedded/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/inc/msp432p401r_classic.h:
+
+/home/mose/Desktop/courses/Embedded/simplelink_msp432p4_sdk_3_40_01_02/source/third_party/CMSIS/Include/core_cm4.h:
+
+/home/mose/Desktop/courses/Embedded/simplelink_msp432p4_sdk_3_40_01_02/source/third_party/CMSIS/Include/cmsis_version.h:
+
+/home/mose/Desktop/courses/Embedded/simplelink_msp432p4_sdk_3_40_01_02/source/third_party/CMSIS/Include/cmsis_compiler.h:
+
+/home/mose/Desktop/courses/Embedded/simplelink_msp432p4_sdk_3_40_01_02/source/third_party/CMSIS/Include/cmsis_ccs.h:
+
+/home/mose/Desktop/courses/Embedded/simplelink_msp432p4_sdk_3_40_01_02/source/third_party/CMSIS/Include/mpu_armv7.h:
+
+/home/mose/Desktop/courses/Embedded/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/inc/system_msp432p401r.h:
+
+/home/mose/Desktop/courses/Embedded/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/driverlib/timer_a.h:
+
+/home/mose/Desktop/courses/Embedded/simplelink_msp432p4_sdk_3_40_01_02/source/ti/drivers/power/PowerMSP432.h:
+
+/home/mose/Desktop/courses/Embedded/simplelink_msp432p4_sdk_3_40_01_02/source/ti/drivers/Board.h:
+
